c0051372703281b712a6743b03fd3f7dd9ebd30d
Luis Hernandez <luis777_hdz_11@hotmail.com>
2022-10-20 22:08:13 -0500

    Working images on pages

--- resumetemplate-master/resumetemplate-master/src/components/header/header.js
+++ resumetemplate-master/resumetemplate-master/src/components/header/header.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+function refreshPage() {
+  setTimeout(()=>{
+      window.location.reload(false);
+  }, 500);
+  console.log('page to reload')
+}
 export default class Header extends Component {
+  
   render() {
     let resumeData = this.props.resumeData;
+    
     return (
       <React.Fragment>
       {/*generated code
@@ -11,11 +20,11 @@ export default class Header extends Component {
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
-            <li><Link to="/Portfolio">Projects</Link></li>
-            <li><Link to="/Resume">Resume</Link></li>
-            <li><Link to="/Contactus">Contacts</Link></li>
-              
+            <li><Link to="/Portfolio" onClick={refreshPage}>Projects</Link></li>
+            <li><Link to="/Resume " onClick={refreshPage}>Resume</Link></li>
+            <li><Link to="/Contactus" onClick={refreshPage}>Contacts</Link></li>
             </ul>
+          
          </nav>
 
       </React.Fragment>
